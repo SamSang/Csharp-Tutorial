f3cacbf6bdfb1132fbcd4fbfd813cc0eb2f807d3
SamSang <akapeterser@gmail.com>
2018-05-06 00:24:45 -0400

    Ran debug, now lots of files were created. Let's add them in...

--- Project.cs
+++ Project.cs
@@ -13,22 +13,22 @@ namespace TeleprompterConsole
             {
                 Console.Writeline(line)
             }
-            //comment?
-            //IEnumerable is an interface defined in System.Collections.Generic
-            //This is an Iterator method
-            //They return sequences that are evaluated lazily -- But what does Lazy mean?
-            static IEnumerable<string> ReadFrom(string file)
+        }
+        //comment?
+        //IEnumerable is an interface defined in System.Collections.Generic
+        //This is an Iterator method
+        //They return sequences that are evaluated lazily -- But what does Lazy mean?
+        static IEnumerable<string> ReadFrom(string file)
+        {
+            string line;
+            //File is a class class is defined in System.IO 
+            //var means you make a variable
+            //reader is thus an Implicitly typed local variable. It's defined at compile time.
+            using (var reader = File.OpenText(file))
             {
-                string line;
-                //File is a class class is defined in System.IO 
-                //var means you make a variable
-                //reader is thus an Implicitly typed local variable. It's defined at compile time.
-                using (var reader = File.OpenText(file))
+                while ((line = reader.ReadLine()) != null)
                 {
-                    while ((line = reader.ReadLine())) != null)
-                    {
-                        yield return line;
-                    }
+                    yield return line;
                 }
             }
         }
